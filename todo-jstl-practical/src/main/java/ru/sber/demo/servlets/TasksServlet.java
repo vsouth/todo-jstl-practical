f005a42ddb0b0c63139933b535e1086fd4a6cd50
vsouth <neppchela@gmail.com>
2024-02-21 00:30:50 +0300

    added tasks servlet (it is working but not functional)

---
+++ todo-jstl-practical/src/main/java/ru/sber/demo/servlets/TasksServlet.java
@@ -0,0 +1,24 @@
+package ru.sber.demo.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/tasks")
+public class TasksServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/login");
+            return;
+        }
+        req.getServletContext()
+                .getRequestDispatcher("/pages/tasks.jsp")
+                .forward(req, resp);
+    }
+}
